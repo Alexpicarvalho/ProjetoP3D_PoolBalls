2da2b41cc35ba9d7e9d21c45233d9c29f6c5091f
Jonny <jonmikel_2000@hotmail.com>
2023-06-01 12:13:07 +0100

    [Update] balls added
    not loading just added them to the repo, started working in the Load.h

--- Load.h
+++ Load.h
@@ -1,1 +1,30 @@
 #pragma once
+
+#include <string>
+#include <glm\glm.hpp>
+
+namespace Load {
+	class Obj
+	{
+	public:
+		void Read(const std::string);
+		void Send(void);
+		void Draw(glm::vec3, glm::vec3);
+
+	};
+
+	void Obj::Read(const std::string obj_model_filepath)
+	{
+
+	}
+
+	void Obj::Send(void)
+	{
+
+	}
+
+	void Obj::Draw(glm::vec3 position, glm::vec3 orientation)
+	{
+
+	}
+}
